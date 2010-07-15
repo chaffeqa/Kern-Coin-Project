683e754180e3b439f20f5398f309e32f964a5439
chaffeqa <chaffeqa@gmail.com>
2010-07-15 13:45:19 -0400

    added main_images to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,20 @@ class Item < ActiveRecord::Base
   belongs_to :category
   has_one :sale
   has_many :cart_items
+  has_attached_file :main_image,
+    :url  => "/images/items/:name/main_image_:style.:extension",
+    :path => ":rails_root/public/images/items/:name/main_image_:style.:extension",
+    :styles => { :thumb => '100x100#' }
 
+  validates_attachment_presence :main_image
+	validates_attachment_size :main_image, :less_than => 2.megabytes
+  validates_attachment_content_type :main_image
   validates_presence_of :name, :item_id, :cost
   validates_numericality_of :cost
 
 
   scope :get_for_sale, where(:for_sale => true)
-
-
+  
   def short_details
     return self.details[0,30] << "..."
   end
