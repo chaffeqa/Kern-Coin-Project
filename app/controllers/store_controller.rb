60578d71fdf29890544fe7843921e6ff989dda1a
chaffeqa <chaffeqa@gmail.com>
2010-07-16 12:18:52 -0400

    working on sort

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,7 +9,8 @@ class StoreController < ApplicationController
   def list
     @items = Item.get_for_sale.displayed
     @items = @items.where(:category_id => params[:category_id]) if params[:category_id]
-    order_by = params[:order_by].to_s.gsub(/\\/, '\&\&').gsub(/'/, "''") if params[:order_by] || 'title'
+    order_by = "title"
+    order_by = params[:order_by].to_s.gsub(/\\/, '\&\&').gsub(/'/, "''") if params[:order_by]
     if params[:asc] and params[:asc] == "desc"
       @asc = "asc"
       @items = @items.order("#{order_by} asc")
