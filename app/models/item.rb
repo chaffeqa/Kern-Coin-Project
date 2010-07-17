ce27a87ffad8e665e23ae6482e1ebae50ff5a411
chaffeqa <chaffeqa@gmail.com>
2010-07-16 20:14:11 -0400

    updated default item main_image

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,11 @@ class Item < ActiveRecord::Base
   has_attached_file :main_image,
     :url  => "/images/items/:id/main_image_:style.:extension",
     :path => ":rails_root/public/images/items/:id/main_image_:style.:extension",
-    :styles => {:display => ['250x250#', :gif], :thumb => ['80x80#', :gif],
-    :default_url => "/images/items/no_image.gif" }
+    :styles => {:display => ['250x250#', :gif], :thumb => ['80x80#', :gif] },
+    :default_url => "/images/items/no_image.gif"
 
-#  validates_attachment_size :main_image, :less_than => 2.megabytes
-#  validates_attachment_content_type :main_image, :content_type => [ 'image/jpeg', 'image/png', 'image/gif', 'image/x-png', 'image/pjpeg' ]
+  #  validates_attachment_size :main_image, :less_than => 2.megabytes
+  #  validates_attachment_content_type :main_image, :content_type => [ 'image/jpeg', 'image/png', 'image/gif', 'image/x-png', 'image/pjpeg' ]
   validates_presence_of :name, :item_id, :cost
   validates_numericality_of :cost
 
