c285a7e3d72f2a2f8383be07af260df42f2a764a
Quinn on VM <chaffeqa@gmail.com>
2010-10-21 14:49:40 -0400

    fixed element ordering and element deletion

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -3,31 +3,28 @@ class ElementsController < ApplicationController
   before_filter :get_node
   
   def destroy
-    @element_destroy.destroy
-    respond_to do |format|
-      format.html { redirect_to shortcut_path(@node.shortcut) }
-      format.js
-    end
+    @current_element.destroy
+    redirect_to( shortcut_path(@node.shortcut), :notice => 'Element successfully Destroyed')
   end
 
   def move_up
     @element = Element.find(params[:id])
     @element.move_higher
-    redirect_to shortcut_path(@node.shortcut)
+    redirect_to( shortcut_path(@node.shortcut), :notice => 'Element successfully Moved')
   end
 
   def move_down
     @element = Element.find(params[:id])
     @element.move_lower
-    redirect_to shortcut_path(@node.shortcut)
+    redirect_to( shortcut_path(@node.shortcut), :notice => 'Element successfully Moved')
   end
 
 
   private
   def get_node
     if params[:id]
-      @element_destroy = Element.find(params[:id])
-      @node = @element_destroy.dynamic_page.node
+      @current_element = Element.find(params[:id])
+      @node = @current_element.dynamic_page.node
     end
     super
   end
