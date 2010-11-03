4f30ae4c66979a161e90fc7af2e803f7dc0b946c
Quinn on VM <chaffeqa@gmail.com>
2010-11-03 05:09:24 -0400

    fixed inventory search

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,28 +1,33 @@
 class InventoryController < ApplicationController
-  
-  def category
-    @category = Category.find( params[:id]) if params[:id]
-    @category = Category.where(:title => 'Inventory').first unless params[:id]
-  end
+  helper_method :sort_column, :sort_direction
+
 
   def list
     @items = Item.get_for_sale.displayed
-    @items = @items.where(:category_id => params[:category_id]) if params[:category_id] and not params[:category_id].empty?
-    @items = @items.where(["name LIKE ? OR details LIKE ?", params[:search_text], params[:search_text]]) if params[:search_text] and not params[:search_text].empty?
-    @items = @items.where(["cost <= ?", params[:cost_range]]) if params[:cost_range] and not params[:cost_range].empty?
-    order_by = "name"
-    order_by = params[:sort_by].to_s.gsub(/\\/, '\&\&').gsub(/'/, "''") if params[:sort_by]
-    if params[:asc] and params[:asc] == "desc"
-      @asc = "asc"
-      @items = @items.order("#{order_by} asc")
-    else
-      @asc = "desc"
-      @items = @items.order("#{order_by} desc")
-    end
+    @items = @items.joins(:node).where('nodes.parent_id' => Node.select(:id).where(:page_type => 'Category', :page_id => params[:category_id])) unless params[:category_id].blank?
+    @items = @items.where(["name LIKE ? OR details LIKE ?", params[:search_text], params[:search_text]]) unless params[:search_text].blank?
+    @items = @items.where(["cost <= ?", params[:cost_range]]) unless params[:cost_range].blank?
+    @items = @items.order(sort_column + " " + sort_direction)
+    @items = @items.paginate :page => params[:page]
+
+    
+    @params = params
+    @params.delete('action')
+    @params.delete('controller')
+    @params.delete('utf8')
+    @params.delete('commit')
+    @params.delete('sort')
+    @params.delete('direction')
   end
 
-  def item
-    @item = Item.find(params[:id])
+  private
+
+  def sort_column
+    Item.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
  
