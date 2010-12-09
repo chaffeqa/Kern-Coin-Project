098ea4093730c703864f14448b279b5d08e5c5e5
Quinn on VM <chaffeqa@gmail.com>
2010-12-09 13:51:39 -0500

    search changes

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -6,7 +6,7 @@ class InventoryController < ApplicationController
     @items = Item.get_for_sale.displayed
     @items = @items.joins(:nodes).where('nodes.parent_id' => Node.select(:id).where(:page_type => 'Category', :page_id => params[:category_id])) unless params[:category_id].blank?
     @items = @items.where(["name LIKE ? OR details LIKE ?", "%"+params[:search_text]+"%", "%"+params[:search_text]+"%"]) unless params[:search_text].blank?
-    @items = @items.where(["cost <= ?", params[:cost_range]]) unless params[:cost_range].blank?
+    @items = @items.where(["cost BETWEEN ? AND ?", low_cost, high_cost]) unless params[:cost_range].blank?
     @items = @items.order(sort_column + " " + sort_direction)
     @items = @items.paginate :page => params[:page]
 
@@ -30,6 +30,14 @@ class InventoryController < ApplicationController
   def sort_direction
     @direction = @direction || params[:direction] || ''
     "ASC DESC".include?(@direction) ? @direction : "ASC"
+  end
+
+  def low_cost
+    params[:cost_range].split('-')[0] || 0
+  end
+
+  def high_cost
+    params[:cost_range].split('-')[1] || 1000000000
   end
 
  
