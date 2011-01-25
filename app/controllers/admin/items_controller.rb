95dfec174334bbed9cfacd09dd8fc1e02e3b59f5
Quinn on VM <chaffeqa@gmail.com>
2011-01-25 10:18:41 -0500

    persisting params in item management

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,7 +5,6 @@ class Admin::ItemsController < ApplicationController
   before_filter :persist_filter_parameters
   
   def index
-    @per_page = params[:per_page] || 10
     @items = Item.scoped
     @items = @items.scope_display(params[:displayed]) unless params[:displayed].blank?
     @items = @items.scope_for_sale(params[:for_sale]) unless params[:for_sale].blank?
@@ -55,41 +54,53 @@ class Admin::ItemsController < ApplicationController
   private
 
   def persist_filter_parameters
-    @parsed_params = {}
-    @parsed_params[:name]=params[:name] unless params[:name].blank?
-    @parsed_params[:item_id]=params[:item_id] unless params[:item_id].blank?
-    @parsed_params[:displayed]=params[:displayed] unless params[:displayed].blank?
-    @parsed_params[:for_sale]=params[:for_sale] unless params[:for_sale].blank?
-    @parsed_params[:min_price]=params[:min_price] unless params[:min_price].blank?
-    @parsed_params[:max_price]=params[:max_price] unless params[:max_price].blank?
-    @parsed_params[:category]=params[:category] unless params[:category].blank?
-    @parsed_params[:per_page]=@per_page || 10
+    create_params_session
+#    @parsed_params = {}
+#    @parsed_params[:name]=session[:name] unless params[:name].blank?
+#    @parsed_params[:item_id]=session[:item_id] unless params[:item_id].blank?
+#    @parsed_params[:displayed]=session[:displayed] unless params[:displayed].blank?
+#    @parsed_params[:for_sale]=session[:for_sale] unless params[:for_sale].blank?
+#    @parsed_params[:min_price]=session[:min_price] unless params[:min_price].blank?
+#    @parsed_params[:max_price]=session[:max_price] unless params[:max_price].blank?
+#    @parsed_params[:category]=session[:category] unless params[:category].blank?
+#    @parsed_params[:per_page]=@per_page || 10
 
 
     @full_params = {}
-    @full_params[:name]=params[:name] unless params[:name].blank?
-    @full_params[:item_id]=params[:item_id] unless params[:item_id].blank?
-    @full_params[:displayed]=params[:displayed] unless params[:displayed].blank?
-    @full_params[:for_sale]=params[:for_sale] unless params[:for_sale].blank?
-    @full_params[:min_price]=params[:min_price] unless params[:min_price].blank?
-    @full_params[:max_price]=params[:max_price] unless params[:max_price].blank?
-    @full_params[:category]=params[:category] unless params[:category].blank?
-    @full_params[:sort]=params[:sort] unless params[:sort].blank?
-    @full_params[:direction]=params[:direction] unless params[:direction].blank?
-    @full_params[:per_page]=@per_page || 10
+    @full_params[:name]=session[:name] unless session[:name].blank?
+    @full_params[:item_id]=session[:item_id] unless session[:item_id].blank?
+    @full_params[:displayed]=session[:displayed] unless session[:displayed].blank?
+    @full_params[:for_sale]=session[:for_sale] unless session[:for_sale].blank?
+    @full_params[:min_price]=session[:min_price] unless session[:min_price].blank?
+    @full_params[:max_price]=session[:max_price] unless session[:max_price].blank?
+    @full_params[:category]=session[:category] unless session[:category].blank?
+    @full_params[:sort]=session[:sort] unless session[:sort].blank?
+    @full_params[:direction]=session[:direction] unless session[:direction].blank?
+    @full_params[:per_page]= session[:per_page]
+
   end
 
-  def get_node
-    super
+  def create_params_session
+    session[:name]=params[:name] unless params[:name].blank?
+    session[:item_id]=params[:item_id] unless params[:item_id].blank?
+    session[:displayed]=params[:displayed] unless params[:displayed].blank?
+    session[:for_sale]=params[:for_sale] unless params[:for_sale].blank?
+    session[:min_price]=params[:min_price] unless params[:min_price].blank?
+    session[:max_price]=params[:max_price] unless params[:max_price].blank?
+    session[:category]=params[:category] unless params[:category].blank?
+    session[:sort]=params[:sort] unless params[:sort].blank?
+    session[:direction]=params[:direction] unless params[:direction].blank?
+    session[:per_page]= params[:per_page] unless params[:per_page].blank?
+    session[:per_page]= 10 if  session[:per_page].blank?
   end
 
   def sort_column
-    @sort = @sort || params[:sort] || ''
+    @sort = @sort || session[:sort] || ''
     Item.column_names.include?(@sort) ? @sort : "name"
   end
 
   def sort_direction
-    @direction = @direction || params[:direction] || ''
+    @direction = @direction || session[:direction] || ''
     "ASC DESC".include?(@direction) ? @direction : "ASC"
   end
 
